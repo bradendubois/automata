ecfda4c525854b7439306032011656520621c8fe
Braden Dubois <drab3n@gmail.com>
2020-05-23 23:49:14 -0600

    Add base WidgetWrap extending classes

--- Automata.py
+++ Automata.py
@@ -2,47 +2,118 @@
 
 import urwid
 
+class AutomataMenu(urwid.WidgetWrap):
+
+    def __init__(self, tabs=None):
+
+        if tabs is None:
+            tabs = urwid.LineBox(urwid.Text("Delta Menu"))
+            
+        super().__init__(tabs)
+        self.tabs = tabs
+        self.tabs.set_title("Automata Menu")
+        self.tabs._selectable = True
+        self.tabs._sizing = "flow"
+        self._sizing = "flow"
+    
+    def keypress(self, size, key):
+        if key == "enter":
+            self.tabs._set_active_by_tab(self)
+        #elif key == "tab":
+        #    self.tabs._set_active_next()
+        #elif key == "shift tab":
+        #    self.tabs._set_active_prev()
+        else:
+            return key
+
+    def selectable(self):
+        return True
+
+class TransitionPanel(urwid.WidgetWrap):
+
+    def __init__(self, w=None):
+        
+        self.show_text = urwid.Text("False")
+
+        if w is None:
+            w = urwid.LineBox(
+                urwid.Pile([
+                    urwid.Text("A"),
+                    urwid.Text("B"),
+                    self.show_text
+                ]))
+
+        super().__init__(w)
+        self._w = w
+        self._w.set_title("Transition Panel")
+        self._w._sizing = "flow"
+        self._w._selectable = True
+        self._sizing = "flow"
+
+    def keypress(self, size, key):
+        if key in ["up", "down", "left", "right"]:
+            return key
+        self.show_text.set_text(key)
+        self.input_area.edit_box.edit_text += key
+
+    def selectable(self):
+        return True
+    
+    def link_input_area(self, input_area):
+        self.input_area = input_area
+class InputArea(urwid.WidgetWrap):
+
+    def __init__(self, widgets=None):
+
+        self.edit_box = urwid.Edit("Enter: ")
+        if widgets is None:
+            widgets = urwid.LineBox(   
+                urwid.ListBox(
+                    urwid.SimpleFocusListWalker([
+                        urwid.Text("Box3"),
+                        urwid.Text("Box4"),
+                        self.edit_box
+                ])))
+
+        super().__init__(widgets)
+        self.widgets = widgets
+        self.widgets.set_title("Input Area")
+        self.widgets._selectable = True
+        self._sizing = "flow"
+    
+    def keypress(self, size, key):
+        if key in ["up", "down", "left", "right"]:
+            return key
+        self.edit_box.edit_text = key
+
+    def selectable(self):
+        return True
+
 def main():
 
     urwid.set_encoding("utf8")
+    
+    automataMenu = AutomataMenu()
+    transitionPanel = TransitionPanel()
 
-    delta_menu = urwid.LineBox(
-        urwid.Text("Delta Menu")
-    )
+    input_area = InputArea()
 
-    transition_menu = urwid.LineBox(
-        
-        urwid.Pile([
-            urwid.Text("A"),
-            urwid.Text("B")
-        ])
-    )
+    transitionPanel.link_input_area(input_area)
 
-
-    main_input_area = urwid.LineBox(
-        
-        urwid.ListBox(
-            urwid.SimpleFocusListWalker([
-                urwid.Text("Box3"),
-                urwid.Text("Box4")
-        ]))
-    )
 
     mainframe = urwid.Pile([
 
-
         urwid.ListBox(
 
             urwid.SimpleFocusListWalker([
 
-                delta_menu,
-
-                transition_menu
+                automataMenu,
+                transitionPanel
 
             ])
         ),
 
-        main_input_area
+        input_area
     ])
 
     def quit(*args, **kwargs):
@@ -51,10 +122,6 @@ def main():
     def handle_key(key):
         if key in ("q", "Q"):
             quit()
-
-    delta_menu.set_title("Delta Transitions")
-    transition_menu.set_title("Transition Panel")
-    main_input_area.set_title("Input Area")
 
     loop = urwid.MainLoop(
         mainframe,
